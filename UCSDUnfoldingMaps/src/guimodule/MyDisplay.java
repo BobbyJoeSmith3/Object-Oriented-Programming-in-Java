6903fa4908067f2421e955136d2eb33bddec41a4
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-12-29 01:46:25 -0600

    Link to background image of badlands in GUI, and drawbackground to PApplet.

--- UCSDUnfoldingMaps/src/guimodule/MyDisplay.java
+++ UCSDUnfoldingMaps/src/guimodule/MyDisplay.java
@@ -1,17 +1,23 @@
 package guimodule;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class MyDisplay extends PApplet {
+	//badlands image url
+	private String URL = "http://images.boomsbeat.com/data/images/full/40877/39-jpg.jpg";
+	private PImage webImg = loadImage(URL, "jpg");
 	
 	public void setup()
 	{
 		size(600, 600);
 		background(170, 63, 57);
 		
+		
 	}
 
 	public void draw()
 	{
+		image(webImg, 0, 0);
 		ellipse(width/2, height/2, 300, 300);
 	}
 }
